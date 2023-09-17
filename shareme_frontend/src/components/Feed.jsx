bc034b6e95797f4d4f37c4b464b12377d6b6685a
SanketLakhani <sanketlakhani80@gmail.com>
2023-09-17 22:19:23 +0530

    pins length issue solved

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -31,7 +31,7 @@ const Feed = () => {
   if (loading)
     return <Spinner message="We are adding new ideas to your feed!" />;
 
-  if (!pins.length) return <h2>No Pins Available</h2>;
+  if (!pins?.length) return <h2>No Pins Available</h2>;
   return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 };
 
